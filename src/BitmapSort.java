f674b3f1fd49bfccf6d2141b3d3b7cff6a80f7ea
Apatheticdinosaur <silverkni@gmail.com>
2017-01-12 21:49:29 -0800

    Added implementation of Programming pearl's bitmap sort in java

---
+++ src/BitmapSort.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Andy on 1/12/2017.
+ *
+ */
+
+import java.util.*;
+
+public class BitmapSort {
+
+    private ArrayList<Integer> array;
+    private BitSet map = new BitSet();
+
+    /**
+     * Default constructor for BitmapSort
+     */
+    public BitmapSort() {
+
+    }
+
+    /**
+     * Constructor for existing sets
+     * @param a the set to be sorted
+     */
+    public BitmapSort(ArrayList<Integer> a) {
+        this.array = a;
+    }
+
+    /**
+     * Setter for the array to be sorted
+     * @param a the array to be sorted
+     */
+    public void setArray(ArrayList<Integer> a) {
+        this.array = a;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public ArrayList<Integer> getSorted() {
+
+    }
+
+}
